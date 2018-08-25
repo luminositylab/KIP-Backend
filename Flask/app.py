3c55f47aa1c6cf28dab481c53c8c45d4d237d4c8
GowanR <gowan.rowland@gmail.com>
2018-08-24 20:48:05 -0700

    added api skeleton.

---
+++ Flask/app.py
@@ -0,0 +1,54 @@
+#####################################################################
+#                                                                   #
+#   `app.py` is the main server for controlling KIP using REST API  #
+#   This software is for educational purposes.                      #
+#                                                                   #
+#   License: https://www.mozilla.org/en-US/MPL/                     #
+#   Written by GowanR <g@gmichaelrowland.com>                       #
+#                                                                   #
+#####################################################################
+
+
+
+from flask import Flask
+from flask import Flask, flash, jsonify, redirect, Response, render_template, request, session, abort
+import os
+import json
+import time
+from access_control import crossdomain
+from BufferedStepperPacket import BufferedStepperPacket
+from ServerResponses import SUCCESS, BAD_QUERYSTRING
+app = Flask(__name__)
+
+
+
+@app.route('/') 
+def index():
+    return SUCCESS
+
+@app.route('/set/servo/<int:servo_id>/<float:servo_position>')
+@crossdomain(origin="*")
+def set_servo_position(servo_id, servo_position):
+    # set servo to position
+    return SUCCESS
+
+@app.route('/get/sensor/<int:sensor_id>')
+@crossdomain(origin="*")
+def get_sensor_data(sensor_id):
+    # get and send sensor data
+    return SUCCESS
+
+@app.route('/set/motor/motionprofile/<int:motor_id>')
+@crossdomain(origin="*")
+def set_motor_motion_profile(motor_id):
+    # should get querystrings and send them through serial in a rate-limited way.
+    return SUCCESS
+
+@app.route('/set/motor/<int:motor_id>/<int:position>')
+@crossdomain(origin="*")
+def set_motor_position(motor_id, position):
+    # sets motor of specified id to specified position in steps
+    retun SUCCESS
+
+if __name__ == '__main__':     
+    app.run(debug=True, host='0.0.0.0')
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # KIP-Backend
+
 All of the code that runs on the raspberry pi and arduino on the robot.
+
+TODO: 
+- Servo arduino code
+- Motion profiler buffer sending
+- Rate limited serial calls (flask side)
+- Absolute position motor control
+- API documentation
+
+
+
+
+ ___________                    ______________
+|           |                  |              |
+|  Client   | <==== HTTP ====> | Flask Server |
+|___________|                  |______________|
+                                     ||
+                                     || Serial
+                                     ||
+                                     \/
+                                 __________
+                                |          |
+                                | Arduino  |
+                                |__________|
