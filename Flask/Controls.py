df6a71d9e7791d1dc6378aa0573984965b34736c
gowanr <gowan.rowland@gmail.com>
2018-11-14 20:10:03 +0000

    Beginning of day commit 11-14-18

--- Flask/Controls.py
+++ Flask/Controls.py
@@ -14,6 +14,7 @@ import threading
 import time
 from time import sleep
 from queue import Queue
+import math
 
 class StateManager(threading.Thread):
     def __init__(self, queue, kwargs=None):
@@ -26,12 +27,17 @@ class StateManager(threading.Thread):
         left_drive_motor = Motor(0)
         right_drive_motor = Motor(1)
         left_drive_motor.reverse()
-
+        left_drive_motor.max_steps_per_packet = 25
+        right_drive_motor.max_steps_per_packet = 25
         left_shoulder_motor = Motor(2)
         right_shoulder_motor = Motor(3)
-        
+         
         left_shoulder_motor.set_reduction(10)
         right_shoulder_motor.set_reduction(10)
+        left_shoulder_motor.reverse()
+        right_shoulder_motor.max_steps_per_packet = 50
+        left_shoulder_motor.max_steps_per_packet = 50
+        base = DriveBase(left_drive_motor, right_drive_motor)
         # end user set up code
 
         while(True):
@@ -42,10 +48,22 @@ class StateManager(threading.Thread):
                 pass
             # begin user defined code
             
-            #left_drive_motor.set(user_inputs['left_drive'])
-            #right_drive_motor.set(user_inputs['right_drive'])
+            left_drive_motor.set(user_inputs['left_drive'])
+            right_drive_motor.set(user_inputs['right_drive'])
             
-            left_shoulder_motor.change_angle(-90) 
+            #left_shoulder_motor.change_angle(-90)
+            
+            #for i in range(5):
+            #    right_drive_motor.move_revolutions(1)
+            #    left_drive_motor.move_revolutions(1)
+            #    sleep(3)
+            #base.drive_forward(3)
+            #right_shoulder_motor.change_angle(90)
+            #left_shoulder_motor.change_angle(90)
+            #sleep(2)
+            #right_shoulder_motor.change_angle(-90)
+            #left_shoulder_motor.change_angle(-90)
+            
             #for i in range(10):
             #    left_drive_motor.set(25)
             #    right_drive_motor.set(25)
@@ -56,10 +74,7 @@ class StateManager(threading.Thread):
             #    right_drive_motor.set(-25)
             
             
-            #print('Right: ', user_inputs['right_drive'], ' Left: ', user_inputs['left_drive'])
-            # left_shoulder_motor.set(user_inputs['left_arm'])
-            # right_shoulder_motor.set(user_inputs['right_arm'])
-
+            
 
 
             # end use defined code
@@ -69,7 +84,8 @@ class StateManager(threading.Thread):
 def degrees_to_steps(theta, reduction): # assume 1.8 degree step
     return theta * (reduction/1.8)
         
-
+def revolutions_to_steps(n, reduction):
+    return (n * 200) / reduction
 def chunks(l, n):
     for i in range(0,len(l), n):
         yield l[i:i+n]
@@ -107,6 +123,10 @@ class Motor:
         step_set = packetize_steps(d_steps, self.max_steps_per_packet)
         self.send_packet(step_set)
     
+    def move_revolutions(self, n):
+        d_steps = revolutions_to_steps(n, self.reduction)
+        step_set = packetize_steps(d_steps, self.max_steps_per_packet)
+        self.send_packet(step_set)
     def change_angle(self, angle):
         d_steps = degrees_to_steps(angle, self.reduction)
         step_set = packetize_steps(d_steps, self.max_steps_per_packet)
@@ -140,8 +160,33 @@ class Motor:
                 if len(p) < 5:
                     for i in range(5-len(p)):
                         p.append(0)
-                BufferedStepperPacket(self.id, 1, d_speed, 1, abs(p[0]) or 0, abs(p[1]) or 0, abs(p[2]) or 0, abs(p[3]) or 0, abs(p[4]) or 0)
+                print("sending packet ", p)
+                BufferedStepperPacket(self.id, 1, d_speed, 0, abs(p[0]) or 0, abs(p[1]) or 0, abs(p[2]) or 0, abs(p[3]) or 0, abs(p[4]) or 0)
         except Exception as inst:
             print(inst)
             return False
         return True
+
+class DriveBase:
+    def __init__(self, left_motor, right_motor):
+        print("init drive base")
+        self.left_motor = left_motor
+        self.right_motor = right_motor
+        self.is_metric = False
+        self.wheel_radius_imperial = 1.37795 # inches
+        self.wheel_radius_metric = 35 ## milimeters
+    def set_metric(self, metric):
+        self.is_metric = metric
+    def distance_to_revolutions(self, n): # metric is boolean
+        if self.is_metric:
+            return (n * 1000)/(2*math.pi*self.wheel_radius_metric)
+        else:
+            return (n * 12)/(2*math.pi*self.wheel_radius_imperial)
+    def drive_forward(self, value): # if imperial, feet, if metric, meters
+        print("running")
+        rev = self.distance_to_revolutions(value)
+            
+        print("revs: ", rev)
+        self.left_motor.move_revolutions(rev)
+        self.right_motor.move_revolutions(rev)
+
