ae932a37c8c880625de39e4e14682aa88a27b1eb
GowanR <gowan.rowland@gmail.com>
2019-06-07 01:20:11 +0000

    feat(server): server update dc release

--- Server/main.py
+++ Server/main.py
@@ -2,9 +2,30 @@
 
 from flask_socketio import SocketIO, emit
 from flask import Flask, render_template, jsonify
+from ATMegaCOM import ATMegaCOM
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+class SimpleRobot:
+    def __init__(self, com):
+        self.left_drive = 0
+        self.right_drive = 0
+        self.arm_pos = 0
+        self.com = com
+    def drive(self, x, y):
+        x = float(x)
+        y = float(y)
+        self.left_drive = x
+        self.right_drive = y
+        self.com.update_robot(self.left_drive, self.right_drive, self.arm_pos)
+    def move_arm(self, v):
+        v = float(v)
+        self.arm_pos = (v + 1)/2
+        self.com.update_robot(self.left_drive, self.right_drive, self.arm_pos)
+        
+com = ATMegaCOM()
+kip = SimpleRobot(com)
 
 def SUCCESS(message=""):
     if message is "":
@@ -20,6 +41,22 @@ def FAILURE(message=""):
 @app.route('/')
 def index():
     return jsonify(SUCCESS())
+
+@app.route('/arm/<value>')
+def arm(value):
+    #print('Got Value: ', value)
+    kip.move_arm(value)
+    return jsonify(SUCCESS())
+
+
+@app.route('/appdrive/<x>/<y>')
+def appdrive(x, y):
+    #print('Got values: ', x, ', ', y)
+    #print('dropped coms: ', com.failed_sends)
+    #print("driving", x, ", ", y)
+    kip.drive(x, y)
+    return jsonify(SUCCESS())
+
 
 @socketio.on('message')
 def handle_message(message):
